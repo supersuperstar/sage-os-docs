12ae03c240507fd39ecd7c336f38410d20211300
Moeakwak <moeakwak@gmail.com>
2022-04-08 15:54:22 +0800

    增加阿里icon

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,7 +7,15 @@ export default defineHopeConfig({
 
   base: "/sage-os-docs/",
 
-  head: [],
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
 
   themeConfig,
 });

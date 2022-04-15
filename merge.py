c1c46cff2c1f528943b05800a1d35c219147fcef
Moeakwak <moeakwak@gmail.com>
2022-04-15 22:44:07 +0800

    merge.py

---
+++ merge.py
@@ -0,0 +1,50 @@
+import os
+import re
+
+
+def read_md(path):
+    result = {}
+    with open(path, 'r') as f:
+        result['content'] = f.read()
+        result["title"] = re.search(r"# (.*)\n", result['content'])[1]
+    return result
+
+
+def merge_chapter(chapter_dir_path):
+    result = {"children": []}
+    for dirpath, dirnames, filenames in os.walk(chapter_dir_path):
+        print(dirpath, dirnames, filenames)
+        for filename in filenames:
+            if filename.endswith(".md"):
+                path = os.path.join(dirpath, filename)
+                md = read_md(path)
+                if filename == "README.md":
+                    result['title'] = md['title']
+                    result['content'] = md['content']
+                else:
+                    md['content'] = md['content'].replace("# ", "## ")
+                    result["children"].append(md)
+    result['children'] = sorted(result['children'], key=lambda x: x['title'])
+    return result
+
+
+def merge(docs_path):
+    result = []
+    dirpath, dirnames, filenames = list(os.walk(docs_path))[0]
+    for dir in dirnames:
+        if dir.startswith("."):
+            continue
+        result.append(merge_chapter(os.path.join(dirpath, dir)))
+    result = sorted(result, key=lambda x: x['title'])
+    text = ""
+    for c in result:
+        text += c['content'] + "\n"
+        for child in c['children']:
+            text += child["content"] + "\n"
+    return text
+
+
+if __name__ == "__main__":
+    res = merge("docs")
+    with open("merge.md", "w") as f:
+        f.write(res)
